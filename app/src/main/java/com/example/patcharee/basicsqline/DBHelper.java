a563ef22c033aeee486fbbe8111a98b6048547f7
BowPatcharee <bbowz233@gmail.com>
2019-01-16 11:29:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/patcharee/basicsqline/DBHelper.java
@@ -0,0 +1,27 @@
+package com.example.patcharee.basicsqline;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String dbName = "krkirk.sqlite";
+    private static final String tableName = "Customers";
+    private static final int dbVersion = 1;
+
+    public DBHelper( Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE " + tableName + "(CustomerID INTEGER PRIMARY KEY,FUIIName TEXT(100),Gender TEXT(1),Address TEXT(200));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
+//main
---
+++ app/src/main/java/com/example/patcharee/basicsqline/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.patcharee.basicsqline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DBHelper db = new DBHelper(this, null, null, 0);
+        db.getWritableDatabase();
+        Toast.makeText(MainActivity.this,"Create New DB success!!",Toast.LENGTH_LONG).show();
+
+
+
+    }
+}
